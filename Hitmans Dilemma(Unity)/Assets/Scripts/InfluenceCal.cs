c724d8139ae9c3cead7ac40026421861ca482736
Sam4Valor <32625124+Sam4Valor@users.noreply.github.com>
2020-10-04 15:23:59 -0400

    Influence
    Created an influence calculator script and level complete script that interacts with Influence calculator. The influence calculator determines the difficulty of each mobs levels as the player defeats them.

---
+++ Hitmans Dilemma(Unity)/Assets/Scripts/InfluenceCal.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfluenceCal : MonoBehaviour
+{
+
+    [Header("Mafia Influence")]
+
+    [Header("IRA")]
+    [Range(0, 100)]
+    public int IRAInfluence = 100;
+    public int IRALevelsDefeated = 0;
+
+    [Header("Italian")]
+    [Range(0, 100)]
+    public int ItalianInfluence = 100;
+    public int ItalianLevelsDefeated = 0;
+
+    [Header("Cartel")]
+    [Range(0, 100)]
+    public int CartelInfluence = 100;
+    public int CartelLevelsDefeated = 0;
+
+    [Header("Yakuza")]
+    [Range(0, 100)]
+    public int YakuzaInfluence = 100;
+    public int YakuzaLevelsDefeated = 0;
+
+    [Header("Russian")]
+    [Range(0,100)]
+    public int RussianInfluence = 100;
+    public int RussianLevelsDefeated = 0;
+
+    // arrays for influence levels
+
+    public int[] MostEffected = new int [6];
+    public int[] SecondEffected = new int[6];
+    public int[] ThirdEffected = new int[6];
+    public int[] LeastEffected = new int[6];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MostEffected[0] = 0;
+        MostEffected[1] = 8;
+        MostEffected[2] = 7;
+        MostEffected[3] = 6;
+        MostEffected[4] = 8;
+        MostEffected[5] = 7;
+
+        SecondEffected[0] = 0;
+        SecondEffected[1] = 5;
+        SecondEffected[2] = 4;
+        SecondEffected[3] = 4;
+        SecondEffected[4] = 5;
+        SecondEffected[5] = 6;
+
+        ThirdEffected[0] = 0;
+        ThirdEffected[1] = 4;
+        ThirdEffected[2] = 5;
+        ThirdEffected[3] = 5;
+        ThirdEffected[4] = 4;
+        ThirdEffected[5] = 4;
+
+        LeastEffected[0] = 0;
+        LeastEffected[1] = 3;
+        LeastEffected[2] = 4;
+        LeastEffected[3] = 5;
+        LeastEffected[4] = 3;
+        LeastEffected[5] = 3;
+    }
+
+    public void CalculateInfluence(int Mob, int Deduction)
+    {
+        // Each level will call this script to edit the data, and refer to this script.
+        Mob += Deduction;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Hitmans Dilemma(Unity)/Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LevelComplete : MonoBehaviour
+{
+
+    public InfluenceCal Influence;
+    
+    public string[] Mob = new string [5];
+
+    int deduction;
+
+    int SelectMob;
+
+    int mobSel;
+    int MobIn;
+    // Call LevelsDefeated from InfluenceCal script. example ("ItalianLevelsDefeated", "IRALevelsDefeated", "YakuzaLevelsDefeated", "CartelLevelsDefeated", "RussianLevelsDefeated")
+    int Level;
+
+    public bool Done = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mob[0] = "IRA";
+        Mob[1] = "Yakuza";
+        Mob[2] = "Cartel";
+        Mob[3] = "Russian";
+        Mob[0] = "Italian";
+
+        
+    }
+
+    void MostEffected()
+    {
+        if (Mob[SelectMob] == "Yakuza")
+        {
+            // 
+            mobSel = Influence.YakuzaLevelsDefeated;
+            deduction = Influence.MostEffected[mobSel];
+
+            Influence.YakuzaInfluence -= 20;
+
+            MobIn = Influence.IRAInfluence;
+
+            Influence.CalculateInfluence(MobIn, deduction);
+            Influence.YakuzaLevelsDefeated += 1;
+           
+            SecondEffected(Influence.CartelInfluence);
+            ThirdEffected(Influence.ItalianInfluence);
+            LeastEffected(Influence.RussianInfluence);
+
+            Done = false;
+        }
+
+        if (Mob[SelectMob] == "IRA")
+        {
+            mobSel = Influence.IRALevelsDefeated;
+            deduction = Influence.MostEffected[mobSel];
+
+            Influence.IRAInfluence -= 20;
+
+            MobIn = Influence.CartelInfluence;
+
+            Influence.CalculateInfluence(MobIn, deduction);
+            Influence.IRALevelsDefeated += 1;
+
+            SecondEffected(Influence.ItalianInfluence);
+            ThirdEffected(Influence.RussianInfluence);
+            LeastEffected(Influence.YakuzaInfluence);
+        }
+
+        if (Mob[SelectMob] == "Italian")
+        {
+            mobSel = Influence.ItalianLevelsDefeated;
+            deduction = Influence.MostEffected[mobSel];
+
+            Influence.ItalianInfluence -= 20;
+
+            MobIn = Influence.RussianInfluence;
+
+
+            Influence.CalculateInfluence(MobIn, deduction);
+            Influence.ItalianLevelsDefeated += 1;
+
+            SecondEffected(Influence.YakuzaInfluence);
+            ThirdEffected(Influence.IRAInfluence);
+            LeastEffected(Influence.CartelInfluence);
+
+        }
+        if (Mob[SelectMob] == "Russian")
+        {
+            mobSel = Influence.RussianLevelsDefeated;
+            deduction = Influence.MostEffected[mobSel];
+
+            Influence.RussianInfluence -= 20;
+
+            MobIn = Influence.YakuzaInfluence;
+
+
+            Influence.CalculateInfluence(MobIn, deduction);
+            Influence.RussianLevelsDefeated += 1;
+
+            SecondEffected(Influence.IRAInfluence);
+            ThirdEffected(Influence.CartelInfluence);
+            LeastEffected(Influence.ItalianInfluence);
+        }
+
+        if (Mob[SelectMob] == "Cartel")
+        {
+            mobSel = Influence.CartelLevelsDefeated;
+            deduction = Influence.MostEffected[mobSel];
+
+            Influence.CartelInfluence -= 20;
+
+            MobIn = Influence.ItalianInfluence;
+
+
+            Influence.CalculateInfluence(MobIn, deduction);
+            Influence.RussianLevelsDefeated += 1;
+
+            SecondEffected(Influence.RussianInfluence);
+            ThirdEffected(Influence.YakuzaInfluence);
+            LeastEffected(Influence.IRAInfluence);
+        }
+    }
+
+    void SecondEffected(int SecondInfluence)
+    {
+        deduction = Influence.SecondEffected[mobSel];
+        MobIn = SecondInfluence;
+        Influence.CalculateInfluence(MobIn, deduction);
+    }
+
+    void ThirdEffected(int ThirdInfluence)
+    { 
+        deduction = Influence.ThirdEffected[mobSel];
+        MobIn = ThirdInfluence;
+        Influence.CalculateInfluence(MobIn, deduction);
+    }
+
+    void LeastEffected(int LeastInfluence)
+    {
+        deduction = Influence.LeastEffected[mobSel];
+        MobIn = LeastInfluence;
+        Influence.CalculateInfluence(MobIn, deduction);
+    }
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Done)
+        {
+            MostEffected();
+        }
+    }
+}
